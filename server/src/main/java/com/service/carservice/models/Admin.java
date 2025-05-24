4e55825459addd24e24d4a37a54384e1e366bcda
IT24102743 <IT24102743@my.sliit.lk>
2025-05-24 13:32:20 +0530

    admin management updates done

---
+++ server/src/main/java/com/service/carservice/models/Admin.java
@@ -0,0 +1,12 @@
+package com.service.carservice.models;
+
+public class Admin extends User {
+
+    public Admin() {
+    }
+
+    public Admin(int id, String name, String email) {
+        super(id, name, email);
+    }
+
+}
---
+++ server/src/main/java/com/service/carservice/services/AdminService.java
@@ -0,0 +1,48 @@
+package com.service.carservice.services;
+
+import org.springframework.stereotype.Service;
+
+import com.service.carservice.models.Admin;
+import com.service.carservice.repositories.AdminRepository;
+
+@Service
+public class AdminService extends BaseService<Admin> {
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository) {
+        super(adminRepository.getAll());
+        this.adminRepository = adminRepository;
+    }
+
+    public Admin[] getAllAdmins() {
+        Admin[] itemsArr = new Admin[items.size()];
+        for (int i = 0; i < items.size(); i++) {
+            itemsArr[i] = items.get(i);
+        }
+        return itemsArr;
+    }
+
+    public void addAdmin(Admin admin) {
+        admin.setId(adminRepository.getNextId(true));
+        items.add(admin);
+    }
+
+    public Admin getAdminById(int id) {
+        return getById(id);
+    }
+
+    public boolean updateAdminById(int id, Admin updatedAdmin) {
+        updatedAdmin.setId(id);
+        return updateById(id, updatedAdmin);
+    }
+
+    @Override
+    protected int getId(Admin admin) {
+        return admin.getId();
+    }
+
+    @Override
+    protected void persistOnShutdown() {
+        adminRepository.persistToFile(items);
+    }
+}
